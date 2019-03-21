11975b5a25ffac71f1c13a4987fce2bc71f31357
Anna Burley <anna.burley@sky.uk>
2019-03-21 11:15:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'blah';
+
+const upperCaseCustomerName = () => {
+  customerName = customerName.toUpperCase();
+  return customerName;
+};
+
+const setBestCustomer = () => {
+  bestCustomer = 'not bob';
+  return bestCustomer;
+};
+
+const overwriteBestCustomer = () => {
+  bestCustomer = 'maybe bob';
+  return bestCustomer;
+};
+
+
+const changeLeastFavoriteCustomer = () => {
+  const leastFavoriteCustomer = 'someone else';
+  return leastFavoriteCustomer;
+};
