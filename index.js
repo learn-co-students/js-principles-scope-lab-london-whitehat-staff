66b69ff67d66c506274ad909927516ef3972df29
JadenVidal <jaden.vidal@sky.uk>
2019-03-21 11:06:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'jaden';
+
+const upperCaseCustomerName = () =>{
+ customerName = customerName.toUpperCase();
+}
+
+const setBestCustomer = () =>{
+    bestCustomer = 'not bob';
+}
+
+const overwriteBestCustomer = () =>{
+    bestCustomer = 'maybe bob';
+}
+
+const changeLeastFavoriteCustomer = () =>{
+    leastFavoriteCustomer = 'not jaden';
+}
